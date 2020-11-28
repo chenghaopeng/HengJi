d5c4e066cca36847aa0b52de761991ec087c0e44
chenghaopeng <i@chper.cn>
2020-11-28 23:00:23 +0800

    真机搜索广播成功

--- app/src/main/java/cn/chper/hengji/MainActivity.java
+++ app/src/main/java/cn/chper/hengji/MainActivity.java
@@ -53,11 +53,16 @@ public class MainActivity extends AppCompatActivity {
         bluetoothAdapter = bluetoothManager.getAdapter();
         bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
         bluetoothLeAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
-        if (bluetoothManager == null || bluetoothAdapter == null || bluetoothLeAdvertiser == null || bluetoothLeScanner == null) {
-            MyToast.show(this, "请打开蓝牙后再使用！");
-            this.finish();
-            return;
-        }
+//        if (bluetoothManager == null || bluetoothAdapter == null || bluetoothLeAdvertiser == null || bluetoothLeScanner == null) {
+//            MyToast.show(this, "请打开蓝牙后再使用！");
+//            Log.d("[蓝牙]", "ERROR!");
+//            Log.d("[蓝牙]", bluetoothManager.toString());
+//            Log.d("[蓝牙]", bluetoothAdapter.toString());
+//            Log.d("[蓝牙]", bluetoothLeScanner.toString());
+//            Log.d("[蓝牙]", bluetoothLeAdvertiser.toString());
+//            this.finish();
+//            return;
+//        }
         refreshUic();
         startScan();
     }
@@ -71,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startAdvertise(String uuid) {
-//        if (bluetoothManager == null || bluetoothAdapter == null || bluetoothLeAdvertiser == null) return;
+        if (bluetoothLeAdvertiser == null) return;
         AdvertiseSettings.Builder advertiseSettingsBuilder = new AdvertiseSettings.Builder();
         advertiseSettingsBuilder.setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY);
         advertiseSettingsBuilder.setTimeout(0);
@@ -104,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void stopAdvertise() {
+        if (bluetoothLeAdvertiser == null) return;
         bluetoothLeAdvertiser.stopAdvertising(advertiseCallback);
     }
 
@@ -136,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
             byte[] scanRecord = result.getScanRecord().getBytes();
-            int startIndex = 2;
+            int startIndex = 0;
             boolean iBeaconFound = false;
             while (startIndex <= 5) {
                 if (((int) scanRecord[startIndex + 2] & 0xff) == 0x02 && ((int) scanRecord[startIndex + 3] & 0xff) == 0x15) {
@@ -148,15 +154,16 @@ public class MainActivity extends AppCompatActivity {
             if (!iBeaconFound) return;
             int major = (scanRecord[startIndex + 20] & 0xff) * 0x100 + (scanRecord[startIndex + 21] & 0xff);
             int minor = (scanRecord[startIndex + 22] & 0xff) * 0x100 + (scanRecord[startIndex + 23] & 0xff);
+            Log.d("[接收到的 major 和 minor]", major + " " + minor);
             if (major != 7947 || minor != 8036) return;
             byte[] uuidByte = new byte[16];
             System.arraycopy(scanRecord, startIndex + 4, uuidByte, 0, 16);
             StringBuilder uuidBuilder = new StringBuilder();
             for (int i = 0; i < 16; ++i) {
-                int x = scanRecord[i] & 0xff;
+                int x = uuidByte[i] & 0xff;
                 String y = Integer.toHexString(x);
                 if (y.length() < 2) uuidBuilder.append("0");
-                uuidBuilder.append(x);
+                uuidBuilder.append(y);
             }
             String uuidRaw = uuidBuilder.toString();
             String uuid = uuidRaw.substring(0, 8) + "-" + uuidRaw.substring(8, 12) + "-" + uuidRaw.substring(12, 16) + "-" + uuidRaw.substring(16, 20) + "-" + uuidRaw.substring(20);
@@ -198,6 +205,11 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<SimpleResponse> call, Response<SimpleResponse> response) {
                 if (response.isSuccessful() && response.body().code == 0) {
                     writeUic(uic);
+                    Log.d("[写入]", uic);
+                }
+                else {
+
+                    Log.d("[有问题]", uic);
                 }
             }
 
